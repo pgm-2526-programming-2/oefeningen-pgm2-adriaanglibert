47550c69b80c6ec92a7f517b612a27f9316b0871
adriaan-glibert-development <info@adriaanglibert.be>
2025-12-10 15:35:11 +0100

    ✨ Basic setup express

---
+++ week_4/server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const myServer = express();
+
+myServer.use(bodyParser.urlencoded({ extended: false }))
+myServer.use(bodyParser.json())
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+function getCurrentTime() {
+    const now = new Date();
+    return `${now.getUTCHours() + 1}:${now.getUTCMinutes()}`;
+}
+
+myServer.get('/api', (request, response) => {
+    console.log('Verzoek op de server!');
+    response.status(200);
+    // response.send(`De tijd op de server is: ${getCurrentTime()}`);
+    response.json({
+        name: "API Test!",
+        requestTime: getCurrentTime()
+    });
+});
+
+// Maak een /api/recipes endpoint! Geef hier alle recepten terug!
+
+myServer.listen(PORT, HOST, () => {
+    console.log(`Server staat aan op http://${HOST}:${PORT}!`); // Afsluiten door Ctrl + C
+});
+
+console.log('Server bestand!');
